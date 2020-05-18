0d716d81b6e10747e76e7e1b3a01eda4bf83b24d
PauloHerculano <paulosz@icloud.com>
2020-05-17 22:08:34 -0300

    Update cumbucaserver.js

--- cumbucaserver.js
+++ cumbucaserver.js
@@ -14,6 +14,19 @@ app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
 
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PATCH, PUT, DELETE, OPTIONS, PUSH, LISTEN"
+    );
+    next();
+  });
+
 const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
   version: '2019-07-12',
   authenticator: new IamAuthenticator({
