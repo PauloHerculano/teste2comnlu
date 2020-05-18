4d068521b82bf97d73f8a7b732e7971a965b2eaf
PauloHerculano <paulosz@icloud.com>
2020-05-17 22:16:42 -0300

    Update cumbucaserver.js

--- cumbucaserver.js
+++ cumbucaserver.js
@@ -1,10 +1,11 @@
 const NaturalLanguageUnderstandingV1 = require('ibm-watson/natural-language-understanding/v1');
 const { IamAuthenticator } = require('ibm-watson/auth');
 var express = require('express');
+var cors = require('cors')
 var app = express();
 var content;
 
-app.get('/nlu', async function (req, res) {
+app.get('/nlu', cors(corsOptionsDelegate), async function (req, res) {
   content = req.query.text;
   var result = await analyzer(content);
   res.send(result);
@@ -14,18 +15,16 @@ app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
 
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-    );
-    res.setHeader(
-      "Access-Control-Allow-Methods",
-      "GET, POST, PATCH, PUT, DELETE, OPTIONS, PUSH, LISTEN"
-    );
-    next();
-  });
+var whitelist = ['http://localhost:4200', 'http://example2.com']
+var corsOptionsDelegate = function (req, callback) {
+  var corsOptions;
+  if (whitelist.indexOf(req.header('Origin')) !== -1) {
+    corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
+  } else {
+    corsOptions = { origin: false } // disable CORS for this request
+  }
+  callback(null, corsOptions) // callback expects two parameters: error and options
+}
 
 const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
   version: '2019-07-12',
